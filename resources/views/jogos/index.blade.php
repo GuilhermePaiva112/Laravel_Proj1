ab91eee55fffeff08fbc39af89997da4995dd6da
Guilherme de Paiva <olaamigo1308@gmail.com>
2024-02-15 19:06:46 -0300

    Working on App/

--- resources/views/jogos/index.blade.php
+++ resources/views/jogos/index.blade.php
@@ -12,31 +12,17 @@
                         <th>Id</th>
                         <th>Nome do Jogo</th>
                         <th>Descrição</th>
-                        <th>Imagem</th>
                         <th>Publicado</th>
                         <th>Ação</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($registros as $registro)
-                    <tr>
-                        <td>{{ $registro->id }}</td>
-                        <td>{{ $registro->titulo }}</td>
-                        <td>{{ $registro->descricao }}</td>
-                        <td><img width="150" src="{{ asset($registro->imagem) }}" alt="{{ $registro->titulo }}" /></td>
-                        <td>{{ $registro->publicado }}</td>
-                        <td>
-                            <a class="btn deep-orange" href="{{ route('admin.cursos.editar',$registro->id) }}">Editar</a>
-                            <a class="btn red" href="{{ route('admin.cursos.deletar',$registro->id) }}">Deletar</a>
-                        </td>
-                    </tr>
-                    @endforeach
+
                 </tbody>
             </table>
         </div>
 
         <div class="row">
-            <a class="btn blue" href="{{ route('admin.cursos.adicionar') }}">Adicionar</a>
 
         </div>
     </div>
